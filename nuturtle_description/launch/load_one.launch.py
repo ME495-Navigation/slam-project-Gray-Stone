99db4f203f7b7169471e9f52ffe750fe2aacfa8b
leo-chen <S8O9R6@u.northwestern.edu>
2024-01-14 23:37:18 -0600

    Task A.5 complete, all 4 colors showing up

--- nuturtle_description/launch/load_one.launch.py
+++ nuturtle_description/launch/load_one.launch.py
@@ -5,6 +5,7 @@ from launch.actions.set_launch_configuration import SetLaunchConfiguration, Some
 from launch.substitutions.command import Command
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.substitutions import ExecutableInPackage
+from launch_ros.descriptions import ParameterValue
 from launch.substitutions.path_join_substitution import PathJoinSubstitution
 from launch.substitutions import EqualsSubstitution, TextSubstitution
 from launch.conditions import IfCondition
@@ -30,9 +31,9 @@ def generate_launch_description() -> LaunchDescription:
                               choices=["red", "green", "blue", "purple"],
                               description="select the color of the turtlebot."),
 
-        # These are alternative ways of concatenate argument into strings 
+        # These are alternative ways of concatenate argument into strings
         # if not wanting to use the concatenation in Node's argument parameter.
-        
+
         # SetLaunchConfiguration("rviz_file_name",
         #                        ["basic_", LaunchConfiguration("color"), ".rviz"]),
         # SetLaunchConfiguration("rviz_base_frame", [
@@ -41,21 +42,23 @@ def generate_launch_description() -> LaunchDescription:
         # ]),
 
         # Nodes to launch
-        Node(package='robot_state_publisher',
-             executable='robot_state_publisher',
-             namespace=LaunchConfiguration("color"),
-             parameters=[{
-                 'robot_description':
-                     Command([
-                         ExecutableInPackage("xacro", "xacro"), " ",
-                         PathJoinSubstitution([
-                             FindPackageShare('nuturtle_description'),
-                             "urdf/turtlebot3_burger.urdf.xacro"
-                         ]), " ", "color:=",
-                         LaunchConfiguration("color")
-                     ]),
-                 'frame_prefix': [LaunchConfiguration("color"), "/"]
-             }]),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            namespace=LaunchConfiguration("color"),
+            parameters=[{
+                'robot_description':
+                    ParameterValue(  # ########## Citation [1] ########## #
+                        Command([
+                            ExecutableInPackage("xacro", "xacro"), " ",
+                            PathJoinSubstitution([
+                                FindPackageShare('nuturtle_description'),
+                                "urdf/turtlebot3_burger.urdf.xacro"
+                            ]), " ", "color:=",
+                            LaunchConfiguration("color")
+                        ])),
+                'frame_prefix': [LaunchConfiguration("color"), "/"]
+            }]),
         Node(package='joint_state_publisher',
              executable="joint_state_publisher",
              namespace=LaunchConfiguration("color"),
@@ -64,7 +67,7 @@ def generate_launch_description() -> LaunchDescription:
             package='rviz2',
             namespace=LaunchConfiguration("color"),
             executable='rviz2',
-            arguments=[["-f", LaunchConfiguration("color"), "/base_link"],
+            arguments=[["-f", LaunchConfiguration("color"), "/base_footprint"],
                        [
                            "-d",
                            PathJoinSubstitution(
